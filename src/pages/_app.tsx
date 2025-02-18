0a0b98c8ecf272c906966d36502daed9a9a5c332
Piamias Victor <chocowalletdev@proton.me>
2025-02-18 22:29:56 +0100

    push

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,8 @@ import "../styles/globals.css";
 import { SegmentationProvider } from "@/contexts/segmentation/SegmentationContext";
 import { FilterProvider } from "@/contexts/FilterContext";
 import { useEffect } from "react";
-import { PharmacyProvider } from "../contexts/segmentation/PharmaciesContext";
-import Layout from "@/components/layout/layout";
+import { PharmacyProvider } from "@/contexts/segmentation/PharmaciesContext";
+import Layout from "@/components/layout/Layout";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
